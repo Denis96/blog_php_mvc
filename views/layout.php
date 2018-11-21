fa4810fe79f163c34db32b64de439a0d56e78855
Tsuki <Tsuki@192.168.12.92>
2018-11-21 16:34:28 +0100

    update

---
+++ views/layout.php
@@ -0,0 +1,21 @@
+<DOCTYPE html>
+<html>
+
+	<head>
+	</head>
+
+	<body>
+
+		<header>
+			<a href='/2daw/M07UF2/teoria/blog_php_mvc'>Home</a>
+			<a href='?controller=posts&action=index'>Posts</a>
+		</header>
+
+		<?php require_once('routes.php'); ?>
+
+		<footer>
+			Copyright
+		</footer>
+
+	</body>
+</html>
